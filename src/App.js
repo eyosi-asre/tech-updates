43f513d13fc9601ea16fdeba285463254ab582d4
eyosi-asre <eyosi8asre@gmail.com>
2025-02-07 15:34:22 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TechUpdates from './TechUpdates';
+
+function App() {
+  return (
+    <div>
+      <TechUpdates />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/svgo.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    plugins: [
+      {
+        name: 'preset-default',
+        params: {
+          overrides: {
+            // customize default plugin options
+          },
+        },
+      },
+      // add more plugins if needed
+    ],
+  };

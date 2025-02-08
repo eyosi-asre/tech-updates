49b33d28cf61f931c2ca00adaf61ceb1c27d0145
eyosi-asre <eyosi8asre@gmail.com>
2025-02-08 21:45:09 +0300

    moved the components

---
+++ src/button.js
@@ -0,0 +1,7 @@
+const Button = ({ children, ...props }) => (
+  <button className="bg-blue-500 text-white p-2 rounded" {...props}>
+    {children}
+  </button>
+);
+
+export default Button;
--- src/component/ui/button.js
+++
@@ -1,1 +0,0 @@
-export const Button = ({ children, ...props }) => <button className="bg-blue-500 text-white p-2 rounded" {...props}>{children}</button>;
